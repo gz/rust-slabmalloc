ed1b877d76fa9bcf3b3e0facbaa06ef52ed32601
Gerd Zellweger <mail@gerdzellweger.com>
2021-05-23 18:57:07 -0700

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -18,7 +18,7 @@
 //! # Implementing GlobalAlloc
 //! See the [global alloc](https://github.com/gz/rust-slabmalloc/tree/master/examples/global_alloc.rs) example.
 #![allow(unused_features)]
-#![cfg_attr(feature = "unstable", feature(const_fn))]
+#![cfg_attr(feature = "unstable", feature(const_fn, const_mut_refs))]
 #![cfg_attr(
     test,
     feature(
@@ -28,7 +28,6 @@
         c_void_variant,
         core_intrinsics,
         vec_remove_item,
-        const_mut_refs
     )
 )]
 #![no_std]
